7121e07d56f34a8a5afefad371f8c20c9a3a23ca
Sunil <sonuame@aol.com>
2017-06-04 00:31:41 +0530

    Add project files.

---
+++ MovieIndexer/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace MovieIndexer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        mDatabaseEntities db = new mDatabaseEntities();
+        List<ScanPath> ScanPaths = new List<ScanPath>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ScanPaths = db.ScanPaths.ToList();
+        }
+
+        private void btnUpdateDB_Click(object sender, RoutedEventArgs e)
+        {
+            List<Movy> Movies = new List<Movy>();
+            foreach (ScanPath item in ScanPaths)
+            {
+                Movies.AddRange(Directory.EnumerateDirectories(item.Path, "*.*", SearchOption.TopDirectoryOnly).Select(m=>new Movy() {
+                    Id = Movies.Count + 1, 
+                    Location = item.Location,
+                    Path = m,
+                    Title = System.IO.Path.GetFileNameWithoutExtension(m)
+                }));
+            }
+
+            db.Movies.RemoveRange(db.Movies);
+            db.Movies.AddRange(Movies);
+            db.SaveChanges();
+
+            grdMovies.AutoGenerateColumns = true;
+            grdMovies.ItemsSource = Movies;
+
+
+        }
+    }
+}
