5af84ca96f9538b2fe6c8bdf31f605e31628a59b
Sunil <sonuame@aol.com>
2017-06-04 23:52:02 +0530

    Completed

--- MovieIndexer/MainWindow.xaml.cs
+++ MovieIndexer/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using System.Collections.ObjectModel;
 
 namespace MovieIndexer
 {
@@ -24,15 +25,19 @@ namespace MovieIndexer
     {
         mDatabaseEntities db = new mDatabaseEntities();
         List<ScanPath> ScanPaths = new List<ScanPath>();
+        List<Movy> Movies = new List<Movy>();
+        
         public MainWindow()
         {
             InitializeComponent();
             ScanPaths = db.ScanPaths.ToList();
+            grdMovies.AutoGenerateColumns = true;
+            LoadGrid();
         }
 
         private void btnUpdateDB_Click(object sender, RoutedEventArgs e)
         {
-            List<Movy> Movies = new List<Movy>();
+            
             foreach (ScanPath item in ScanPaths)
             {
                 Movies.AddRange(Directory.EnumerateDirectories(item.Path, "*.*", SearchOption.TopDirectoryOnly).Select(m=>new Movy() {
@@ -47,10 +52,36 @@ namespace MovieIndexer
             db.Movies.AddRange(Movies);
             db.SaveChanges();
 
-            grdMovies.AutoGenerateColumns = true;
+            LoadGrid();
+
+        }
+
+        private void grdMovies_Loaded(object sender, RoutedEventArgs e)
+        {
+            lblStat.Content = "Total Movies - " + grdMovies.Items.Count;
+        }
+
+        public async void LoadGrid()
+        {
+            await Task<List<Movy>>.Run(() => {
+                Movies = db.Movies.ToList();
+            });
             grdMovies.ItemsSource = Movies;
+            lblStat.Content = "Total Movies - " + grdMovies.Items.Count;
+        }
 
+        private void txtTitle_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                grdMovies.ItemsSource = Movies.Where(m=>m.Title.ToLower().Contains(txtTitle.Text.ToLower()));
+            }
+        }
 
+        private void grdMovies_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Movy m = grdMovies.SelectedItem as Movy;
+            System.Diagnostics.Process.Start(m.Path);
         }
     }
 }
---
+++ MovieIndexer/MovieItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieIndexer
+{
+    public class MovieItem : Movy, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
